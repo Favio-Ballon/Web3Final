eff1b941aabb95a10f3fde2bc9a0cc9cd5ee6870
favio-ballon <favioballon@gmail.com>
2025-07-04 14:57:40 -0400

    feat: update configuracion routes y add nuevas rutas

--- frontend/src/navigation/RouterConfig.tsx
+++ frontend/src/navigation/RouterConfig.tsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from 'react-router-dom';
+import { URLS } from './CONSTANTS';
+import UsersRoutes from '../pages/gestionUsuarios';
+import VotacionRoutes from '../pages/sistemaVotacion';
+import PadronElectoralRoutes from '../pages/padrónElectoral';
+import AdminElectoralRoutes from '../pages/administracionElectoral';
 
-import { URLS } from "./CONSTANTS";
-
-const RouterConfig = () => {
-    return (
-        <Routes>
-            <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-    );
+export default function RouterConfig() {
+  return (
+    <Routes>
+        <Route path={URLS.HOME} element={<div>Inicio</div>} />
+        <Route path={URLS.LOGIN} element={<div>Login</div>} />
+        <Route path={URLS.GESTIONUSUARIOS} element={<UsersRoutes />} />
+        <Route path={URLS.PADRONELECTORAL} element={< PadronElectoralRoutes />} />
+        <Route path={URLS.ADMINISTRACIONELECTORAL} element={<AdminElectoralRoutes />} />
+        <Route path={URLS.VOTACION} element={<VotacionRoutes />} />
+        <Route path={URLS.NOT_FOUND} element={<div>Página no encontrada</div>} />
+    </Routes>
+  );
 }
-export default RouterConfig;
