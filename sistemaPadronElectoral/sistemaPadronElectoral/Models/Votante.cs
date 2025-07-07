b16c48a11074f66f8358df0833ae71e041aa7ee3
favio-ballon <favioballon@gmail.com>
2025-07-07 03:54:44 -0400

    Add Votante class and update DbContext for EF support
    Introduces the `Votante` class in the `SistemaPadronElectoral.Models` namespace with properties for voter details, including a GUID identifier. Updates `SistemaPadronElectoralContext` to include a `DbSet` for `Votante` and specifies precision for `Latitud` and `Longitud` properties in the model configuration.

---
+++ sistemaPadronElectoral/sistemaPadronElectoral/Models/Votante.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaPadronElectoral.Models
+{
+    public class Votante
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Codigo { get; set; }
+        public int Ci { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Foto { get; set; }
+        public string CiReverso { get; set; }
+        public string CiAnverso { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
+        public decimal Latitud { get; set; }
+
+        public decimal Longitud { get; set; }
+
+        public Votante()
+        {
+            Codigo = Guid.NewGuid();
+        }
+    }
+}
