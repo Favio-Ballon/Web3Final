0f78e32b997c9c8f0cd90aa2f0af574d2ca4c428
favio-ballon <favioballon@gmail.com>
2025-07-07 03:53:51 -0400

    Add VotantesController and VotanteDto for API handling
    This commit introduces a new `VotantesController` class to manage HTTP requests for "Votante" entities in the ASP.NET Core application. The controller includes methods for retrieving, updating, creating, and deleting votantes, along with support for file uploads (photos and identification cards). Additionally, a new `VotanteDto` class is created to define the data transfer object for creating and updating votantes, with validation rules applied to its properties.

---
+++ sistemaPadronElectoral/sistemaPadronElectoral/Controllers/VotantesController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaPadronElectoral.Models;
+using SistemaPadronElectoral.data;
+using sistemaPadronElectoral.Dtos;
+
+namespace sistemaPadronElectoral.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VotantesController : ControllerBase
+    {
+        private readonly SistemaPadronElectoralContext _context;
+        private readonly string _uploadsFolder = Path.Combine("wwwroot", "uploads");
+
+        public VotantesController(SistemaPadronElectoralContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Votantes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Votante>>> GetVotante()
+        {
+            return await _context.Votante.ToListAsync();
+        }
+
+        // GET: api/Votantes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Votante>> GetVotante(Guid id)
+        {
+            var votante = await _context.Votante.FindAsync(id);
+
+            if (votante == null)
+            {
+                return NotFound();
+            }
+
+            return votante;
+        }
+
+        // PUT: api/Votantes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVotante(Guid id, [FromForm] VotanteDto votanteDto)
+        {
+            var votante = await _context.Votante.FindAsync(id);
+            if (votante == null)
+            {
+                return NotFound();
+            }
+
+            votante.Ci = votanteDto.Ci;
+            votante.Nombre = votanteDto.Nombre;
+            votante.Apellido = votanteDto.Apellido;
+            votante.Direccion = votanteDto.Direccion;
+            votante.FechaNacimiento = votanteDto.FechaNacimiento;
+
+            Directory.CreateDirectory(_uploadsFolder);
+
+            if (votanteDto.Foto != null)
+            {
+                var fotoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.Foto.FileName));
+                using (var stream = new FileStream(fotoPath, FileMode.Create))
+                {
+                    await votanteDto.Foto.CopyToAsync(stream);
+                }
+                votante.Foto = fotoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+            if (votanteDto.CiReverso != null)
+            {
+                var ciReversoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.CiReverso.FileName));
+                using (var stream = new FileStream(ciReversoPath, FileMode.Create))
+                {
+                    await votanteDto.CiReverso.CopyToAsync(stream);
+                }
+                votante.CiReverso = ciReversoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+            if (votanteDto.CiAnverso != null)
+            {
+                var ciAnversoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.CiAnverso.FileName));
+                using (var stream = new FileStream(ciAnversoPath, FileMode.Create))
+                {
+                    await votanteDto.CiAnverso.CopyToAsync(stream);
+                }
+                votante.CiAnverso = ciAnversoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+
+            _context.Entry(votante).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VotanteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Votantes
+        [HttpPost]
+        public async Task<ActionResult<Votante>> PostVotante([FromForm] VotanteDto votanteDto)
+        {
+            Directory.CreateDirectory(_uploadsFolder);
+
+            var votante = new Votante
+            {
+                Ci = votanteDto.Ci,
+                Nombre = votanteDto.Nombre,
+                Apellido = votanteDto.Apellido,
+                Direccion = votanteDto.Direccion,
+                FechaNacimiento = votanteDto.FechaNacimiento,
+                Latitud = votanteDto.Latitud,
+                Longitud = votanteDto.Longitud
+            };
+
+            if (votanteDto.Foto != null)
+            {
+                var fotoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.Foto.FileName));
+                using (var stream = new FileStream(fotoPath, FileMode.Create))
+                {
+                    await votanteDto.Foto.CopyToAsync(stream);
+                }
+                votante.Foto = fotoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+            if (votanteDto.CiReverso != null)
+            {
+                var ciReversoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.CiReverso.FileName));
+                using (var stream = new FileStream(ciReversoPath, FileMode.Create))
+                {
+                    await votanteDto.CiReverso.CopyToAsync(stream);
+                }
+                votante.CiReverso = ciReversoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+            if (votanteDto.CiAnverso != null)
+            {
+                var ciAnversoPath = Path.Combine(_uploadsFolder, Guid.NewGuid() + Path.GetExtension(votanteDto.CiAnverso.FileName));
+                using (var stream = new FileStream(ciAnversoPath, FileMode.Create))
+                {
+                    await votanteDto.CiAnverso.CopyToAsync(stream);
+                }
+                votante.CiAnverso = ciAnversoPath.Replace("wwwroot", string.Empty).Replace("\\", "/");
+            }
+
+            _context.Votante.Add(votante);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVotante", new { id = votante.Codigo }, votante);
+        }
+
+        // DELETE: api/Votantes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVotante(Guid id)
+        {
+            var votante = await _context.Votante.FindAsync(id);
+            if (votante == null)
+            {
+                return NotFound();
+            }
+
+            _context.Votante.Remove(votante);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VotanteExists(Guid id)
+        {
+            return _context.Votante.Any(e => e.Codigo == id);
+        }
+    }
+}
---
+++ sistemaPadronElectoral/sistemaPadronElectoral/Dtos/VotanteDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sistemaPadronElectoral.Dtos
+{
+    public class VotanteDto
+    {
+        [Required(ErrorMessage = "El campo CI es obligatorio.")]
+        public int Ci { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El campo Dirección es obligatorio.")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "La foto es obligatoria.")]
+        public IFormFile Foto { get; set; }
+        [Required(ErrorMessage = "El CI reverso es obligatorio.")]
+        public IFormFile CiReverso { get; set; }
+        [Required(ErrorMessage = "El CI anverso es obligatorio.")]
+        public IFormFile CiAnverso { get; set; }
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        public DateOnly FechaNacimiento { get; set; }
+        [Required (ErrorMessage = "La latitud es obligatoria.")]
+        public decimal Latitud { get; set; }
+        [Required(ErrorMessage = "La longitud es obligatoria.")]
+        public decimal Longitud { get; set; }
+    }   
+
+}
